6fe6e9cd0ced91d244a735f9c7f7ebed389c7e9f
AleksandrGrabov <grabov-it@yandex.by>
2023-04-10 19:49:29 +0300

    Examble 005 If Else

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    Console.WriteLine("Введите имя пользователя: ");
+    string username = Console.ReadLine();
+
+    if (username.ToLower() == "маша")
+    {
+      Console.Write("Ура, это же МАША");
+    }
+    else
+    {
+      System.Console.Write("Привет, ");
+      System.Console.WriteLine(username);
+    }
+  }
+}
